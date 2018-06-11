60acb1a8d64ef143e5f10bb386df51a80446091e
Christopher McLaughlin <cmclau01@gmail.com>
2018-06-11 21:58:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function logWhisper (string4) {
 function sayHiToGrandma (string5) {
   var lowercase = "hello";
   var uppercase = "Hello";
+  var mixedCase = "I love you";
   if (string5.toLowerCase() === lowercase) {
     return "I can\'t hear you!";
   } else if (string5.toUpperCase() === uppercase) {
