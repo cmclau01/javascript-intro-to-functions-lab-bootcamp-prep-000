99a37db5e4410c71a6f1f8ff3b301afc0cda24eb
Christopher McLaughlin <cmclau01@gmail.com>
2018-06-11 21:35:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //first test
 function shout (string) {
   return string.toUpperCase();
-};
+}
 //second test
 function whisper (string2) {
   return string2.toLowerCase();
-};
+}
 //third test
 function logShout (string3) {
   console.log(string3.toUpperCase());
-};
+}
 //fourth test
